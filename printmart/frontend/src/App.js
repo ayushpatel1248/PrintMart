17271138e73d29edf43845c0525124a3c42bd54a
ayush <ayushpatel1248@gmail.com>
2024-06-27 11:41:15 +0530

    s

--- printmart/frontend/src/App.js
+++ printmart/frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Routes, BrowserRouter ,useParams  } from "react-router-dom";
 import HomePage from './components/home/HomePage';
+import PrintOut from './components/printoutform/PrintOut';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element= {<HomePage/>} />        
+        <Route path="/printout" element= {<PrintOut/>} />        
       </Routes>
     </BrowserRouter>
   );
