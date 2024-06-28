155f7d932139a5ac3ec2c2391b715e1a4f1de30a
ayush <ayushpatel1248@gmail.com>
2024-06-28 18:16:40 +0530

    s

--- printmart/frontend/src/components/header/Header.jsx
+++ printmart/frontend/src/components/header/Header.jsx
@@ -182,26 +182,26 @@ export default function Header() {
                 >
                   Orders
                 </a>
-                <a
+                <Link to="/aboutus"
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   About Us
-                </a>
+                </Link>
               </div>
               <div className="py-6">
-                <a
-                  href="#"
+                <Link to="/login"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Log in
-                </a>
-                <a
+                </Link>
+                <Link
+                to="/register"
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Sign up
-                </a>
+                </Link>
               </div>
             </div>
           </div>
