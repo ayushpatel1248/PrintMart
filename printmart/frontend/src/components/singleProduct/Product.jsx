1723fd516477fad047dccfda902f1b58081d62f5
ayush <ayushpatel1248@gmail.com>
2024-07-14 22:16:03 +0530

    s

--- printmart/frontend/src/components/singleProduct/Product.jsx
+++ printmart/frontend/src/components/singleProduct/Product.jsx
@@ -164,7 +164,6 @@ export default function Product() {
               <h2 className="text-sm font-medium text-gray-900">Details</h2>
 
               <div className="mt-4 space-y-6">
-                {/* <p className="text-sm text-gray-600">{product.details}</p> */}
               </div>
             </div>
           </div>
